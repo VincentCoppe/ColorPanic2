b6555c41f5274838a49710f15eeeb24d4c6844c5
Florent ROBERT <florent.robert@etu.unice.fr>
2020-06-22 11:26:34 +0200

    #17 player class; added standard assets #23 spikes tp player to the start

---
+++ Color Panic 2/Assets/Script/Block/Items/KillPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
+            other.gameObject.SendMessage("Death");
+        }
+    }
+    
+}
