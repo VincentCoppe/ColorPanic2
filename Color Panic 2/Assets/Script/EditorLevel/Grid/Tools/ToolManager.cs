3240bcbc4e8bdf7872f93537712f0fc9fc8efd24
Barthélemy Passin-Cauneau <34715570+BarthelemyPassinCauneau@users.noreply.github.com>
2020-07-09 15:16:37 +0200

    #8 : spriterenderer de la baguette

--- Color Panic 2/Assets/Script/EditorLevel/Grid/Tools/ToolManager.cs
+++ Color Panic 2/Assets/Script/EditorLevel/Grid/Tools/ToolManager.cs
@@ -7,7 +7,7 @@ public class ToolManager : MonoBehaviour
     [SerializeField] private TileGameObject tile = null;
     [SerializeField] private Size size = null;
     [SerializeField] private HoverBlock hoverBlock = null;
-    [SerializeField] private Sprite sprite = null;
+    private Sprite sprite;
     public static ITool Tool;
 
     protected void SetTool(ITool tool) {
@@ -31,6 +31,7 @@ public class ToolManager : MonoBehaviour
     }
 
     public void DisplayHover(GridManager gridManager, (int,int) mouse) {
+        //sprite = Resources.Load<Sprite>("Sprite/Tools/tools.png");
         //Cursor.SetCursor(sprite.texture, Vector2.zero, CursorMode.ForceSoftware);
         hoverBlock.DisplayCells(gridManager, Tool.GetBlocksToHover(gridManager, tile, size.size, mouse));
     }
