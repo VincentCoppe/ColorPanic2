bd166fe2176795266fc07dfcbe939fcb67899987
Florent ROBERT <florent.robert@etu.unice.fr>
2020-07-09 11:41:38 +0200

    #48 PlayMode test setup, some first player test in play mode

---
+++ Color Panic 2/Assets/Editor/Playmode/PlayerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerTest
+    {
+        [Test]
+        public void Player_MoveX()
+        {
+            GameObject go = new GameObject();
+            go.AddComponent<Rigidbody2D>();
+            PlayerController pc = go.AddComponent<PlayerController>(); 
+            pc.MoveX(-1);
+            Vector3 v = new Vector3(1,0,0);
+            Assert.True(pc.gameObject.transform.localPosition == v);
+        }
+
+        [Test]
+        public void Player_MoveY()
+        {
+            GameObject go = new GameObject();
+            go.AddComponent<Rigidbody2D>();
+            PlayerController pc = go.AddComponent<PlayerController>(); 
+            pc.MoveY(-1);
+            Vector3 v = new Vector3(0,1,0);
+            Assert.True(pc.gameObject.transform.localPosition == v);
+        }
+
+    }
+}
