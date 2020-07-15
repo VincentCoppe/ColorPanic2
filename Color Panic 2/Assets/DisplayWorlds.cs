774f3b723f06e64c6c27192d37c5e1a400bc6677
Florent ROBERT <florent.robert@etu.unice.fr>
2020-07-15 11:36:43 +0200

    #51 preparation for multiple worlds selection

---
+++ Color Panic 2/Assets/DisplayWorlds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayWorlds : MonoBehaviour
+{
+    [SerializeField] List<Canvas> Worlds;
+
+    public void DisplayW1(){
+        Worlds[1].gameObject.SetActive(false);
+        Worlds[0].gameObject.SetActive(true);
+    }
+
+    public void DisplayW2(){
+        Worlds[0].gameObject.SetActive(false);
+        Worlds[1].gameObject.SetActive(true);
+    }
+}
