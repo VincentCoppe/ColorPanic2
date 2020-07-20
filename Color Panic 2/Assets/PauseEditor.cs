55ae8815380469904d0818531b7108bb21cbbaed
Barthélemy Passin-Cauneau <34715570+BarthelemyPassinCauneau@users.noreply.github.com>
2020-07-20 14:32:47 +0200

    #8 : pitites couleurs sur le chrono

--- Color Panic 2/Assets/PauseEditor.cs
+++ Color Panic 2/Assets/PauseEditor.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseEditor : MonoBehaviour
 {
+    [SerializeField] private Image image = null;
     private void Start() {
         Time.timeScale = 0;
+        image.color = new Color(1,0,0);
     }
 
     public void TimePause(){
         if(Time.timeScale == 1){
             Time.timeScale = 0;
+            image.color = new Color(1,0,0);
         } else {
             Time.timeScale = 1;
+            image.color = new Color(0,1,0);
         }
     }
 }
