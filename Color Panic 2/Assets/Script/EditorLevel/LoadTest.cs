86fba1ca6eeeb63f7f7503023a84a8296dac36c7
Barthélemy Passin-Cauneau <34715570+BarthelemyPassinCauneau@users.noreply.github.com>
2020-07-22 15:04:40 +0200

    #64 : messages pour le test dans l'editeur

---
+++ Color Panic 2/Assets/Script/EditorLevel/LoadTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadTest : MonoBehaviour
+{
+    [SerializeField] private LevelManager levelManager = null;
+    [SerializeField] private GameObject windowTestable = null;
+    [SerializeField] private GameObject windowNotTestable = null;
+    [SerializeField] private TMP_Text missingElements = null;
+   
+
+    public void CheckLevelTestable() {
+        if(levelManager.getPlayerPlaced() == new Vector3(-1,-1,-1) || !levelManager.getFinishPlaced()) {
+            windowNotTestable.SetActive(true);
+            string elements = "";
+            if(levelManager.getPlayerPlaced() == new Vector3(-1,-1,-1))
+                elements = "Player";
+            if(!levelManager.getFinishPlaced()) {
+                elements = elements == "" ? "" : elements+"\n";
+                elements += "Finish flag";
+            }
+            missingElements.SetText(elements);
+        } else {
+            windowTestable.SetActive(true);
+        }
+    }
+}
